9407fe40b64304002530700ec420314010618dd1
Risang12 <rabdurrahmang@gmail.com>
2021-08-15 10:07:05 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Mobile nav toggle
+ */
+"click",
+  ".mobile-nav-toggle",
+  function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  };
